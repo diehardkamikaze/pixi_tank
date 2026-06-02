20663ce8bb1d81365ecd35425532f5dae888a4bb
ryan <ryan@wisebits.com>
2026-06-02 22:47:54 +0300

    added textures loading

---
+++ mjs/assetsMap.js
@@ -0,0 +1,20 @@
+export const assetsMap = {
+  sprites: [
+    {
+      alias: "GunConnectorD",
+      src: "../assets/parts/gun_connectors/GunConnectorD.png",
+    },
+
+    { alias: "HeavyGunB", src: "../assets/parts/guns/HeavyGunB.png" },
+
+    { alias: "HeavyHullB", src: "../assets/parts/hulls/HeavyHullB.png" },
+
+    { alias: "HeavyTowerB", src: "../assets/parts/towers/HeavyTowerB.png" },
+
+    { alias: "TrackAFrame1", src: "../assets/parts/tracks/TrackAFrame1.png" },
+
+    { alias: "TrackAFrame2", src: "../assets/parts/tracks/TrackAFrame2.png" },
+
+    { alias: "MediumShell", src: "../assets/parts/bullets/MediumShell.png" },
+  ],
+};
--- mjs/index.js
+++ mjs/index.js
@@ -1,14 +1,17 @@
 import * as PIXI from "./pixi.mjs";
+import { assetsMap } from "./assetsMap.js";
 
 (async () => {
   const app = new PIXI.Application();
 
   await app.init({
-    background: "black",
-    width: 600,
+    background: "gray",
+    width: 800,
     view: document.getElementById("canvas"),
-    height: 600,
+    height: 800,
   });
 
+  const textures = await PIXI.Assets.load(assetsMap.sprites);
+  console.log(textures);
   document.body.appendChild(app.canvas);
 })();
