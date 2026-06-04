125d238ae4dfb610548f2e9a414e6b7986fa4134
ryan <ryan@wisebits.com>
2026-06-04 22:00:07 +0300

    fix for github pages

--- mjs/assetsMap.js
+++ mjs/assetsMap.js
@@ -5,18 +5,18 @@ export const assetsMap = {
       src: "../assets/parts/gun_connectors/GunConnectorD.png",
     },
 
-    { alias: "HeavyGunB", src: "../assets/parts/guns/HeavyGunB.png" },
+    { alias: "HeavyGunB", src: "./assets/parts/guns/HeavyGunB.png" },
 
-    { alias: "HeavyHullB", src: "../assets/parts/hulls/HeavyHullB.png" },
+    { alias: "HeavyHullB", src: "./assets/parts/hulls/HeavyHullB.png" },
 
-    { alias: "HeavyTowerB", src: "../assets/parts/towers/HeavyTowerB.png" },
+    { alias: "HeavyTowerB", src: "./assets/parts/towers/HeavyTowerB.png" },
 
-    { alias: "MediumTowerA", src: "../assets/parts/towers/MediumTowerA.png" },
+    { alias: "MediumTowerA", src: "./assets/parts/towers/MediumTowerA.png" },
 
-    { alias: "TrackСFrame1", src: "../assets/parts/tracks/TrackСFrame1.png" },
+    { alias: "TrackСFrame1", src: "./assets/parts/tracks/TrackСFrame1.png" },
 
-    { alias: "TrackСFrame2", src: "../assets/parts/tracks/TrackСFrame2.png" },
+    { alias: "TrackСFrame2", src: "./assets/parts/tracks/TrackСFrame2.png" },
 
-    { alias: "MediumShell", src: "../assets/parts/bullets/MediumShell.png" },
+    { alias: "MediumShell", src: "./assets/parts/bullets/MediumShell.png" },
   ],
 };
